6d6b1c0eb63b2f7d62cb5102fe95f5df09a465ea
fstipo <fstipanov@gmail.com>
2022-03-22 15:45:54 +0100

    test build

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ function App() {
             </Card>
           </Col>
           <Col>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form className="mb-md-3">
+              <Form.Group className="mb-0" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
@@ -58,7 +58,7 @@ function App() {
           <Breadcrumb.Item>Test</Breadcrumb.Item>
           <Breadcrumb.Item active>Test</Breadcrumb.Item>
         </Breadcrumb>
-        <Alert>This is a button</Alert>
+        <Alert>This is a not a button</Alert>
         <Button className="text-center">Test Button</Button>
       </Container>
     </div>
