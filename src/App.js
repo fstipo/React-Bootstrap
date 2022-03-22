65b1355b5c238a65e4f844874c1338ca671b291d
fstipo <fstipanov@gmail.com>
2022-03-22 11:40:56 +0100

    🚀 first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <Button>Test Button</Button>
+    </div>
+  );
+}
+
+export default App;
